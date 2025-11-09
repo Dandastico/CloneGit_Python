a9dae6be59fe95774bdf667629131e8837f5ff59
Dandastico <dandastico.bsb@gmail.com>
2025-11-09 17:48:37 -0300

    peixe

---
+++ documentacao/plano_de_projeto.md
@@ -0,0 +1,15 @@
+```mermaid
+graph TD;
+    A-->B;
+    A-->C;
+    B-->D;
+    C-->D;
+```
+
+```mermaid
+graph TD;
+    A[Início]-->B[Processo 1];
+    A-->C[Processo 2];
+    B-->D[Fim];
+    C-->D;
+```
---
+++ main.py
@@ -0,0 +1,12 @@
+import argparse
+
+def main():
+    leitor = argparse.ArgumentParser(
+        description="SenacGit: clone do git para Faculdade Senac"
+    )
+
+    
+
+
+if __name__ == "__main__":
+    main()
